856c5bb3e7d914409f7c977b189dee8d2d8e6a8d
Ruchika2696 <ruchi.joshi2696@gmail.com>
2019-04-02 12:21:35 -0700

    Updates

---
+++ precache-manifest.cbe35c1e7e8254817c883ec686a2fcea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "499bb9d92233e5575b30",
+    "url": "/my-app/static/css/main.cf084fbf.chunk.css"
+  },
+  {
+    "revision": "499bb9d92233e5575b30",
+    "url": "/my-app/static/js/main.533b7be1.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "63c42230f3c0ea13f1a6",
+    "url": "/my-app/static/js/2.5c51f050.chunk.js"
+  },
+  {
+    "revision": "9555a360294f12b11d3b4966e41dc27e",
+    "url": "/my-app/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.fbca038f960e9f1638b529a3e8f3a92b.js"
+  "/my-app/precache-manifest.cbe35c1e7e8254817c883ec686a2fcea.js"
 );
 
 workbox.clientsClaim();
