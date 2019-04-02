6ab39134f822f3b8c140ae341cad4d08822169db
Ruchika2696 <ruchi.joshi2696@gmail.com>
2019-04-02 03:02:28 -0700

    Updates

--- precache-manifest.abed3ea7d496bff74c100cb03986d05a.js
+++ precache-manifest.abed3ea7d496bff74c100cb03986d05a.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "757b868ea42c51c7b866",
+    "revision": "4fd5a9cc58a1b7758e85",
     "url": "/my-app/static/css/main.0fc57544.chunk.css"
   },
   {
-    "revision": "757b868ea42c51c7b866",
-    "url": "/my-app/static/js/main.c87901ac.chunk.js"
+    "revision": "4fd5a9cc58a1b7758e85",
+    "url": "/my-app/static/js/main.5e966231.chunk.js"
   },
   {
     "revision": "9930044c31793394defe",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/my-app/static/js/2.c263ffb2.chunk.js"
   },
   {
-    "revision": "40a65a86b1aee4b57ff36a48f0dfadd4",
+    "revision": "0226174e3c69c7a08b76123ee6ca0d79",
     "url": "/my-app/index.html"
   }
 ];
