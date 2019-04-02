76e3af3fb4a8e316321b33f151150a46dd715c9b
Ruchika2696 <ruchi.joshi2696@gmail.com>
2019-04-02 01:33:54 -0700

    first commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import WorldMap from './Components/WorldMap'
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(<Router basename={process.env.PUBLIC_URL}><App /></Router>, document.getElementById('root'));
 
